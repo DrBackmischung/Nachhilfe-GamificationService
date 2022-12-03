fa660bd5cfe7f28cb0f03a26f65ca72b45404722
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-12-03 21:03:12 +0100

    initial

---
+++ src/main/kotlin/de/wi2020sebgroup1/nachhilfe/gamification/GamificationApplication.kt
@@ -0,0 +1,11 @@
+package de.wi2020sebgroup1.nachhilfe.gamification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GamificationApplication
+
+fun main(args: Array<String>) {
+	runApplication<GamificationApplication>(*args)
+}
